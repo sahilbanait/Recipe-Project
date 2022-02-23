c9af2c49206cc93e88d708bb9f68b147a658081e
sahilbanait <77091616+sahilbanait@users.noreply.github.com>
2022-02-23 21:41:20 +0800

    recipe.model.ts added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('brownie','This is a test','https://www.seriouseats.com/thmb/9GYEd9f4O2W3OMWsXcfoWsAG9Fc=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2018__03__20180413-brownie-mix-vicky-wasik-20-585247e1dd334df9849799ad76f29fa7.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
